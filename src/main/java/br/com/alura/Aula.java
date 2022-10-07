22d44a0c9896d8a6739f2186e7a0c6c5e0070e21
Guilherme Henrique Lemes de Godoy <guilherme.hlg.eng@gmail.com>
2022-10-07 07:23:54 -0300

    finaliza capitulo de lista de objetos

---
+++ src/main/java/br/com/alura/Aula.java
@@ -0,0 +1,29 @@
+package br.com.alura;
+
+public class Aula implements Comparable<Aula> {
+    private String titulo;
+    private Integer tempo;
+
+    public Aula(String titulo, Integer tempo) {
+        this.titulo = titulo;
+        this.tempo = tempo;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public Integer getTempo() {
+        return tempo;
+    }
+
+    @Override
+    public String toString() {
+        return "[Aula: " + this.titulo + ", " + this.tempo + " minutos]";
+    }
+
+    @Override
+    public int compareTo(Aula a) {
+        return this.titulo.compareTo(a.getTitulo());
+    }
+}
